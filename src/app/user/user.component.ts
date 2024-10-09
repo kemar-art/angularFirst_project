92c059fbef002e8013fa56b93ed610196b85c9b0
KEMARNORTON <kemarnorton@hotmail.com>
2024-10-08 21:40:32 -0500

    Using Signal with input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
 
 @Component({
   selector: 'app-user',
@@ -12,17 +11,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  
+//  @Input({required: true}) avatar!: string; 
+//  @Input({required: true}) name!: string;
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  //get imagePath() {
-   // return 'assets/users/' + this.selectedUser.avatar
- // }
+//This is using signal for input
+avatar = input.required<string>();
+name = input.required<string>();
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+
+// get imagePath() {
+//   return 'assets/users/' + this.avatar;
+// }
+
+imagePath = computed(() => {
+  return 'assets/users/' + this.avatar();
+} );
+
+  onSelectUser() {}
 
 
 }
