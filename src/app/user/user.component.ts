7336a91a7e4c1f3eff8c1d8c9d5a4767b372300d
KEMARNORTON <kemarnorton@hotmail.com>
2024-10-09 19:47:07 -0500

    Useing Output Fuction instead of Decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,34 +1,38 @@
-import { Component, computed, Input, input } from '@angular/core';
-
-
-
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  output,
+  Output /*input*/,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  
-//  @Input({required: true}) avatar!: string; 
-//  @Input({required: true}) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  //@Output() select = new EventEmitter();
+  select = output<string>();
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+
+  }
+}
 
 //This is using signal for input
-avatar = input.required<string>();
-name = input.required<string>();
-
-
-// get imagePath() {
-//   return 'assets/users/' + this.avatar;
-// }
-
-imagePath = computed(() => {
-  return 'assets/users/' + this.avatar();
-} );
-
-  onSelectUser() {}
-
-
-}
+// avatar = input.required<string>();
+// name = input.required<string>();
+// imagePath = computed(() => {
+//   return 'assets/users/' + this.avatar();
+// } );
